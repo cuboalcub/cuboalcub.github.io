c63e21d430c860dc23075cc81c8f1487dedc73ef
cuboalcub <fvera8192@gmail.com>
2026-02-26 09:19:06 -0600

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,15 @@ export default function Home() {
                   Contactarme
                 </Link>
               </div>
+              <div className="mt-3 sm:mt-0 sm:ml-3">
+                <a
+                  href="/CV_Felipe_Abisai_Vera_Rodriguez.pdf"
+                  download
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-700 bg-green-100 hover:bg-green-200 md:py-4 md:text-lg md:px-10"
+                >
+                  Descargar CV
+                </a>
+              </div>
             </div>
           </div>
         </div>
